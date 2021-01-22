afd898a07267d624d5f20b2a18c27fc9294bdebe
a <a>
2021-01-22 15:02:46 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './components/full-banner/full-banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
